98e7053edb9629f1aa7e647e632e0423da444923
unknown <ryanbfc@live.com>
2020-07-09 14:28:29 -0300

    up

---
+++ Clone Netflix/Netflix/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { FullBannerComponent } from './components/full-banner/full-banner.component';
+import { ListTitlesComponent } from './components/list-titles/list-titles.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { TitleComponent } from './components/title/title.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    FullBannerComponent,
+    ListTitlesComponent,
+    FooterComponent,
+    TitleComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Clone Netflix/Netflix/src/app/components/title/title.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, Input, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-title',
+  templateUrl: './title.component.html',
+  styleUrls: ['./title.component.css']
+})
+export class TitleComponent implements OnInit {
+
+
+
+  @Input() title: any;
+  @ViewChild('trailer', {static: true})trailer: ElementRef<HTMLVideoElement>;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  playVideo(){
+    this.trailer.nativeElement.play();
+  } stopVideo(){
+    
+    this.trailer.nativeElement.currentTime = 0;
+    this.trailer.nativeElement.pause();
+  }
+  getMuted(): boolean{
+    return this.trailer.nativeElement.muted;
+  }
+  toggleSound(){
+    this.trailer.nativeElement.muted = !this.trailer.nativeElement.muted;
+  }
+}
